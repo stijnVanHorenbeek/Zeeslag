e3f274ca85f1e6aad043c5d945c3cab65539da4b
Seyelegna <Pepsitwist13>
2019-01-10 16:32:59 +0100

    fastest one goes to rescue

--- src/main/java/models/Tower.java
+++ src/main/java/models/Tower.java
@@ -2,12 +2,18 @@ package models;
 
 import models.valueTypes.Coordinates;
 
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.LinkedList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class Tower extends Actor implements IObservable {
     private LinkedList<Vehicle> vehicles;
     private LinkedList<Vehicle> vehiclesInRescue;
     private LinkedList<Vehicle> vehiclesInSos;
+    private List<Vehicle> vehiclesSortedByReactionTime;
+
 
     public Tower(Coordinates coordinates, double speed, double reactionTime, double agility, double size, int capacity, double vector) {
         super(coordinates, speed, reactionTime, agility, size, capacity, vector);
@@ -24,16 +30,19 @@ public class Tower extends Actor implements IObservable {
         vehicles.remove(observer);
     }
 
+    @Override
     public void notifyObservers() {
         for (Vehicle vehicleInSos: vehiclesInSos){
             for (Vehicle vehicle: vehicles){
                 if (!vehiclesInSos.contains(vehicle) && !vehiclesInRescue.contains(vehicle)){
-                    if (vehicle.getCapacity()>= vehicleInSos.getCapacity()){
+                    vehiclesSortedByReactionTime= vehicles.stream().sorted(Comparator.comparing(Vehicle::getReactionTime).reversed()).collect(Collectors.toList());
+                    for (Vehicle vehicleByReactionTime: vehiclesSortedByReactionTime){
+                        if (vehicleByReactionTime.getCapacity()>= vehicleInSos.getCapacity()){
                             //notify ships to rescue shipInSos
                             vehicle.update(vehicleInSos);
                             return;
+                        }
                     }
-
                 }
             }
         }
